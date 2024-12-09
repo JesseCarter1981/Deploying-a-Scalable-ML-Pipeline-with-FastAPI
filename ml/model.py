0b49c993ee7cdb3a5da2b24004c5fa2b92f89b56
JesseCarter1981 <jesse.carter1981@gmail.com>
2024-12-09 15:07:16 -0600

    version 1.0.0

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,7 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-from sklearn.linear_model import LogisticRegression
+# TODO: add necessary import
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -19,9 +19,8 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
-    return model
+    # TODO: implement the function
+    pass
 
 
 def compute_model_metrics(y, preds):
@@ -40,9 +39,9 @@ def compute_model_metrics(y, preds):
     recall : float
     fbeta : float
     """
-    fbeta = fbeta_score(y, preds, beta = 1, zero_division = 1)
-    precision = precision_score(y, preds, zero_division = 1)
-    recall = recall_score(y, preds, zero_division = 1)
+    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
+    precision = precision_score(y, preds, zero_division=1)
+    recall = recall_score(y, preds, zero_division=1)
     return precision, recall, fbeta
 
 
@@ -60,8 +59,8 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    preds = model.predict(X)
-    return preds
+    # TODO: implement the function
+    pass
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -73,26 +72,17 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    with open(file_path, 'wb') as f:
-        pickle.dump(instance, f)
+    # TODO: implement the function
+    pass
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    with open(file_path, 'rb') as f:
-        return pickle.load(f)
-
-
+    # TODO: implement the function
+    pass
 
 
 def performance_on_categorical_slice(
-    data,
-    col,
-    slicevalue,
-    cat_features,
-    label,
-    encoder,
-    lb,
-    model
+    data, column_name, slice_value, categorical_features, label, encoder, lb, model
 ):
     """ Computes the model metrics on a slice of the data specified by a column name and
 
@@ -127,18 +117,12 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    #implement the function
-    sliced_data = data[data[col] == slicevalue]
-    X_slice = sliced_data.drop(columns=[label])
-    y_slice = sliced_data[label]
-
+    # TODO: implement the function
     X_slice, y_slice, _, _ = process_data(
-        X_slice,
-        categorical_features = cat_features,
-        label = label,
-        lb = lb, training = False
+        # your code here
+        # for input data, use data in column given as "column_name", with the slice_value 
+        # use training = False
     )
-
-    preds = inference(model, X_slice)
+    preds = None # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
--- train_model.py
+++ train_model.py
@@ -2,7 +2,6 @@ import os
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
 
 from ml.data import process_data
 from ml.model import (
@@ -13,21 +12,17 @@ from ml.model import (
     save_model,
     train_model,
 )
-# load the cencus.csv data
+# TODO: load the cencus.csv data
 project_path = os.getcwd()
 data_path = os.path.join(project_path, 'data', 'census.csv')
 print(data_path)
 data = pd.read_csv('data/census.csv')
 cleaned_data = data.copy()
 
-#Here we are going to split the dataset into both a training and test set.
-train_full, test = train_test_split(cleaned_data, test_size = 0.20, random_state = 42)
+# TODO: split the provided data to have a train dataset and a test dataset
+train, test = None, None# Your code here
 
-# First, we will define the number of folds we want.
-n_splits = 5
-kf = KFold(n_splits = n_splits, shuffle = True, random_state = 42)
-
-#Next, we need to make a Categorical features list
+# DO NOT MODIFY
 cat_features = [
     "workclass",
     "education",
@@ -39,149 +34,54 @@ cat_features = [
     "native-country",
 ]
 
-#Here we will collect the metrecs for each fold.
-all_metrics = []
-
-#Now we are going to process the dataset before the k-fold splitting.
-X, y, encoder, lb = process_data(
-    cleaned_data,
-    categorical_features = cat_features,
-    label = 'salary',
-    training = True
-)
-
-#Here is the K-fold cross validation.
-for train_index, val_index in kf.split(X):
-    X_train, X_val = X[train_index], X[val_index]
-    y_train, y_val = y[train_index], y[val_index]
-
-#Now we will train the model on the fold.
-    model = train_model(X_train, y_train)
-
-    preds = inference(model, X_val)
-
-#Here we are going to compute the metrics.
-    p, r, fb = compute_model_metrics(y_val, preds)
-    all_metrics.append((p, r, fb))
-
-#Now we average the metgrics across all the folds.
-avg_precision = sum(m[0] for m in all_metrics) / n_splits
-avg_recall = sum(m[1] for m in all_metrics) / n_splits
-avg_f1 = sum(m[2] for m in all_metrics) / n_splits
-
-print(f'Average Precision: {avg_precision:.4f} | Average Recall: {avg_recall:.4f} | Average F1: {avg_f1:.4f}')
-
-#Here we are going to process the test dataset.
-X_test, y_test, _, _ = process_data(
-    test,
-    categorical_features = cat_features,
-    label = 'salary',
-    training = False,
-    encoder = encoder,
-    lb = lb,
-)
-
-preds = inference(model, X_test)
-
-# Now we can calculate and print the metrics on the test set.
-p, r, fb = compute_model_metrics(y_test, preds)
-print(f"Test Precision: {p:.4f} | Test Recall: {r:.4f} | Test F1: {fb:.4f}")
-
-#Here we will test the performance on the categorical slices.
-for col in cat_features :
-    for slicevalue in sorted(test[col].unique()):
-        count = test[test[col] == slicevalue].shape[0]
-
-        test_slice = test[test[col] == slicevalue]
-        X_slice, y_slice, _, _ = process_data(
-            test_slice,
-            categorical_features = cat_features ,
-            label = 'salary',
-            training = False,
-            encoder = encoder,
-            lb = lb
-        )
-
-        preds_slice = inference(model, X_slice)
-        p, r, fb = performance_on_categorical_slice(
-            data,
-            col,
-            slicevalue,
-            cat_features,
-            'salary',
-            encoder,
-            lb,
-            model
-        )
-
-        with open("slice_output.txt", "a") as f:
-            print(f"{col}: {slicevalue}, Count: {count:,}", file = f)
-            print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file = f)
-
-
-# Train final model with the entire training data
+# TODO: use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
-    train_full,
-    categorical_features = cat_features,
-    label = 'salary',
-    training = True
-)
+    # your code here
+    # use the train dataset 
+    # use training=True
+    # do not need to pass encoder and lb as input
+    )
 
-model = train_model(X_train, y_train)
-
-# Save the model and encoder
-model_path = os.path.join(project_path, 'model', 'model.pkl')
-save_model(model, model_path)
-encoder_path = os.path.join(project_path, 'model', 'encoder.pkl')
-save_model(encoder, encoder_path)
-
-# Load the model for inference
-model = load_model(model_path)
-
-# Process the test set for final evaluation
 X_test, y_test, _, _ = process_data(
     test,
-    categorical_features = cat_features,
-    label='salary',
+    categorical_features=cat_features,
+    label="salary",
     training=False,
     encoder=encoder,
     lb=lb,
 )
 
-# Use the model to make predictions
-preds = model.predict(X_test)
+# TODO: use the train_model function to train the model on the training dataset
+model = None # your code here
+
+# save the model and the encoder
+model_path = os.path.join(project_path, "model", "model.pkl")
+save_model(model, model_path)
+encoder_path = os.path.join(project_path, "model", "encoder.pkl")
+save_model(encoder, encoder_path)
+
+# load the model
+model = load_model(
+    model_path
+) 
+
+# TODO: use the inference function to run the model inferences on the test dataset.
+preds = None # your code here
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
-print(f'recision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}')
+print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}")
 
-# Compute performance on categorical slices
+# TODO: compute the performance on model slices using the performance_on_categorical_slice function
+# iterate through the categorical features
 for col in cat_features:
+    # iterate through the unique values in one categorical feature
     for slicevalue in sorted(test[col].unique()):
         count = test[test[col] == slicevalue].shape[0]
-
-        # Get the slice for this categorical feature value
-        test_slice = test[test[col] == slicevalue]
-
-        # Process slice data
-        X_slice, y_slice, _, _ = process_data(
-            test_slice,
-            categorical_features = cat_features,
-            label='salary',
-            training=False,
-            encoder=encoder,
-            lb=lb
-        )
-
-        # Make predictions for the slice
-        preds_slice = inference(model, X_slice)
-
-        # Calculate performance metrics for the slice
         p, r, fb = performance_on_categorical_slice(
-            test_slice, col, slicevalue, cat_features, 'salary', encoder, lb, model
+            # your code here
+            # use test, col and slicevalue as part of the input
         )
-
-        # Save the results to a file
-        with open('slice_output.txt', 'a') as f:
-            print(f'{col}: {slicevalue}, Count: {count:,}', file=f)
-            print(f'Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}', file=f)
+        with open("slice_output.txt", "a") as f:
+            print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
+            print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file=f)
