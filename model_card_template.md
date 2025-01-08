0233cad03b5776e40c855226bc49e49b1b634ef6
JesseCarter1981 <jesse.carter1981@gmail.com>
2025-01-08 13:37:00 -0600

    Version 2.0.0

--- model_card_template.md
+++ model_card_template.md
@@ -3,16 +3,33 @@
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
+- This model is created for the ML Machine learning DevOps project part two
+- This model was created and maintained by Jesse Carter a student at Western Governors University
+- Created on 1/08/2025
+- Version 1.0
+- Using the starter code, I wrote a machine learning model that trains on the clean data and saves the model. I completed any function that has been started
+- Wrote unit tests for at least 3 functions in the model code
+- Wrote a function that outputs the performance of the model on slices of the data
+
 
 ## Intended Use
+- This project is intended to show an example of deploying a Scalable ML Pipeline in Production
+- Predict whether income exceeds $50K/yr based on census data
 
-## Training Data
 
-## Evaluation Data
+## Factors
+- This data is very old as it was collected in 1994, as such, it should only be used for instructional purposes
+- It would also be advisable to consider the sampling groups as well
 
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
+There were three metrics used in this project; fbeta_score, precision_score and recall_score.
 
-## Ethical Considerations
+## Evaluation Data
+There was only 20% of the original dataset for evaluation purposes of the model.
 
-## Caveats and Recommendations
+## Training Data
+There was the final 80% of the original dataset for the training purposes of the model.
+
+## Quantitative Analyses
+The model performance is measured based on three metrics: fbeta score, precision score and recall score.
+
--- test_ml.py
+++ test_ml.py
@@ -1,27 +1,65 @@
+import os
 import pytest
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score, recall_score, accuracy_score
+from ml.model import train_model, inference, save_model, load_model, performance_on_categorical_slice, process_data
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+project_path = os.getcwd()
+data_path = os.path.join(project_path, 'data', 'census.csv')
 
+cat_features = [
+    'workclass',
+    'education',
+    'marital-status',
+    'occupation',
+    'relationship',
+    'race',
+    'sex',
+    'native-country',
+]
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
+from sklearn.metrics import precision_score, recall_score, accuracy_score
+def compute_model_metrics(y_true, y_pred):
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    accuracy = accuracy_score(y_true, y_pred)
+    return precision, recall, accuracy
 
+@pytest.fixture
+def model_data():
+    train = pd.read_csv(data_path)
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features = cat_features, label = 'salary', training = True)
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
+    model = train_model(X_train, y_train)
+    return model, X_train, y_train
+
+def test_compute_model_metrics(model_data):
+    model, X_train, y_train = model_data
+    
+    
+    preds = inference(model, X_train)
+    metrics = compute_model_metrics(y_train, preds)
+
+    assert len(metrics) == 3
+    assert isinstance(metrics, tuple)
+
+    for metric in metrics:
+        assert 0 <= metric <= 1
+
+def test_inference(model_data):
+    model, X_train, _ = model_data
+    
+    preds = inference(model, X_train)
+
+    assert len(preds) == len(X_train)
+    
+    assert np.all((preds == 0) | (preds == 1))
+
+def test_train_model(model_data):
+    model, _, _ = model_data
+    
+    assert isinstance(model, RandomForestClassifier)
